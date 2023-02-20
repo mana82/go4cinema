2b4af8fe47cfd244a677228a76adec72bbd25b97
manojef <XASgm4c_yrzC@x2>
2023-02-20 17:37:05 +0200

    update index changes

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+var cors = require("cors");
+
+const db = require('./db')
+const movieRoute = require('./routes/movie-router')
+
+const app = express();
+const port = 8080
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(cors());
+//app.listen(port, () => console.log(`Server running on port ${port}`))
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Go4 Cinema!')
+})
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use('/api', movieRoute)
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
